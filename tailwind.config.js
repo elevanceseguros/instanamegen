602ec55d7c13398ec435e0d06d7b36a176ce84e5
elevanceseguros <elevanceseguros@gmail.com>
2026-05-22 20:57:42 -0300

    add tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./index.html', './src/**/*.{js,jsx}'],
+  theme: {
+    extend: {
+      colors: {
+        brand: {
+          primary: '#6C63FF',
+          secondary: '#FF6584',
+          dark: '#1a1a2e',
+          card: '#16213e',
+        }
+      },
+      fontFamily: {
+        sans: ['Inter', 'system-ui', 'sans-serif'],
+      }
+    },
+  },
+  plugins: [],
+}
